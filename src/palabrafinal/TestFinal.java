5cb86e32605cc5e384e37225a680a14e7103ae9d
gemamm <gemamm1988@gmail.com>
2021-09-30 10:41:42 +0200

    Initial commit

---
+++ src/palabrafinal/TestFinal.java
@@ -0,0 +1,19 @@
+
+package palabrafinal;
+
+
+public class TestFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int valor1 = 10;
+        System.out.println("valor1 = " + valor1);
+        //valor1 = 15;
+        // no se puede modificar el valor de la variable
+        //final
+        
+    }
+    
+}
